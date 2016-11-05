18ab3d61cd2ee669b44d9a2036347752661ab406
agustin-mounier <agusmouni@gmail.com>
2016-11-05 17:56:02 -0300

    added resources to insight service

--- services/insightService.js
+++ services/insightService.js
@@ -3,21 +3,52 @@ var http = require('http');
 var options = {
   host: 'localhost',
   port: 3001,
-  path: '/insight-api/block-index/0',
+  path: '',
   method: 'GET',
   headers: {
       'Content-Type': 'application/json'
   }
 };
 
-var getLastBlock = function(){
-	  var req = http.get(options, function(res) {
-		  console.log('STATUS: ' + res.statusCode);
-		  res.on('data', function (data) {
-		  	return JSON.parse(data);
-		  });
-	});
 
+var GET = function(options, callback) {
+	var req = http.get(options, function(res) {
+		console.log('STATUS: ' + res.statusCode);
+		res.on('data', function (data) {
+			if(callback == undefined) { 
+				console.log(JSON.parse(data));
+			} else {
+				callback(JSON.parse(data));
+			}
+		});
+	});
 }
 
-module.exports.getLastBlock = getLastBlock;
+var getBlockByIndex = function(index) {
+	getBlockHash(index, getBlock);
+}
+
+var getBlockHash = function(index, callback){
+	options.path = '/insight-api/block-index/' + index;
+	GET(options, callback);
+	
+}
+
+var getBlock = function(resp, callback){
+	options.path = '/insight-api/block/' + resp.blockHash;
+	GET(options, callback);
+}
+
+var getTransactionByBlock = function(blockHash, callback){
+	options.path = '/insight-api/txs/?block=' + blockHash;
+	GET(options, callback);
+}
+
+module.exports.getBlockHash = getBlockHash;
+module.exports.getBlock = getBlock;
+module.exports.getBlockByIndex = getBlockByIndex;
+module.exports.getTransactionByBlock = getTransactionByBlock;
+
+
+
+
--- services/predictionService.js
+++ services/predictionService.js
@@ -1,7 +1,10 @@
 var insightService = require('./insightService.js');
 
 var prediction = function(){
-	console.log(insightService.getLastBlock());
+	insightService.getBlockHash(12354321);
+	//insightService.getBlockByIndex(0);
+	insightService.getTransactionByBlock('000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f');
+	//console.log(insightService.getBlock(blockHash));
 }
 
 module.exports.prediction = prediction;
